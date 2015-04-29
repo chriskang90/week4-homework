02d2a41c98099e0f220ba4f62e4c4fb9008ddd79
ck <chriskang@gmail.com>
2015-04-29 12:19:52 -0500

    Initial commit
    Completed changes for the place resource to follow best practices.

--- chicago/app/controllers/places_controller.rb
+++ chicago/app/controllers/places_controller.rb
@@ -1,17 +1,30 @@
+# Description of changes
+# 1. Modified "submitted" method into "create"
+# 2. Included a limit for the index page following best practices
+# 3. Included a handler for non-existent places in show and edit
+# 4. Modified update method to use named routing
+
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    # Include a limit, following best practices
+    @places = Place.all.limit(100)
   end
 
   def show
     @place = Place.find(params[:id])
+
+    # Include a handler if the place does not exist
+    if @place == nil
+      redirect_to places_url, notice: "Place not found"
+    end
+
   end
 
   def new
   end
 
-  def submitted
+  def create
     Place.create title: params[:title],
                  price: (params[:price].to_f * 100).to_i,
                  photo_url: params[:url],
@@ -22,6 +35,12 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    # Include a handler if the place does not exist
+    if @place == nil
+      redirect_to places_url, notice: "Place not found"
+    end
+
   end
 
   def update
@@ -31,7 +50,7 @@ class PlacesController < ApplicationController
                  photo_url: params[:url],
                  desc: params[:desc]
 
-    redirect_to "/places/#{@place.id}"
+    redirect_to place_url(@place.id)
   end
 
   def delete
